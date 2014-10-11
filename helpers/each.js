5da9b507824fa252bbe1e9a126f0ce708d05532f
kjirou <kjirou.web@gmail.com>
2014-10-11 16:10:43 +0900

    Fix a problem that callback isn't called if options empty

--- helpers/each.js
+++ helpers/each.js
@@ -1,6 +1,10 @@
 module.exports = function _each(collection, iterator, cb) {
   var done = 0;
 
+  if (collection.length === 0) {
+    return cb();
+  }
+
   collection.forEach(function(item) {
     iterator(item, function(err) {
       done += 1;
